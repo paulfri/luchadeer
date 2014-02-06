87495a6849b7852fab3b18fb008f34b96957cb84
Paul Friedman <paulrfri@gmail.com>
2014-02-05 22:10:44 -0500

    Clean up Luchadeer::Error a bunch

--- lib/luchadeer/error.rb
+++ lib/luchadeer/error.rb
@@ -1,12 +1,12 @@
 module Luchadeer
   class Error < StandardError
 
+    # http://www.giantbomb.com/api/documentation#toc-0-0
     module Codes
       OK                     = 1
       INVALID_API_KEY        = 100
       OBJECT_NOT_FOUND       = 101
       URL_FORMAT_ERROR       = 102
-      JSON_CALLBACK_REQUIRED = 103 # should be only apply to 'jsonp' format
       FILTER_ERROR           = 104
       SUBSCRIBER_ONLY        = 105
     end
@@ -14,12 +14,11 @@ module Luchadeer
     # Used for bespoke status codes returned in the response body
     def self.api_errors
       {
-        Codes::INVALID_API_KEY => Luchadeer::Error::InvalidAPIKey,
-        Codes::OBJECT_NOT_FOUND => Luchadeer::Error::ObjectNotFound,
-        Codes::URL_FORMAT_ERROR => Luchadeer::Error::URLFormatError,
-        Codes::JSON_CALLBACK_REQUIRED => Luchadeer::Error::JSONCallbackRequired,
-        Codes::FILTER_ERROR => Luchadeer::Error::FilterError,
-        Codes::SUBSCRIBER_ONLY => Luchadeer::Error::SubscriberOnly
+        Codes::INVALID_API_KEY        => Luchadeer::Error::InvalidAPIKey,
+        Codes::OBJECT_NOT_FOUND       => Luchadeer::Error::NotFound,
+        Codes::URL_FORMAT_ERROR       => Luchadeer::Error::URLFormatError,
+        Codes::FILTER_ERROR           => Luchadeer::Error::FilterError,
+        Codes::SUBSCRIBER_ONLY        => Luchadeer::Error::SubscriberOnly
       }
     end
 
@@ -27,36 +26,25 @@ module Luchadeer
     def self.http_errors
       {
         400 => Luchadeer::Error::BadRequest,
-        401 => Luchadeer::Error::Unauthorized,        # unconfirmed
-        403 => Luchadeer::Error::Forbidden,           # unconfirmed
         404 => Luchadeer::Error::NotFound,
-        408 => Luchadeer::Error::RequestTimeout,      # unconfirmed
         500 => Luchadeer::Error::InternalServerError,
-        502 => Luchadeer::Error::BadGateway,          # unconfirmed
-        503 => Luchadeer::Error::ServiceUnavailable,  # unconfirmed
-        504 => Luchadeer::Error::GatewayTimeout       # unconfirmed
+        502 => Luchadeer::Error::InternalServerError, # unconfirmed
+        503 => Luchadeer::Error::InternalServerError, # unconfirmed
+        504 => Luchadeer::Error::InternalServerError, # unconfirmed
       }
     end
 
   private
 
+    class RedirectLimitReached < Luchadeer::Error; end
+    class RequestTimeout < Luchadeer::Error; end
+
     class InvalidAPIKey < Luchadeer::Error; end
-    class ObjectNotFound < Luchadeer::Error; end
     class URLFormatError < Luchadeer::Error; end
-    class JSONCallbackRequired < Luchadeer::Error; end
     class FilterError < Luchadeer::Error; end
     class SubscriberOnly < Luchadeer::Error; end
-
-    class RedirectLimitReached < Luchadeer::Error; end
-
     class BadRequest < Luchadeer::Error; end
-    class Unauthorized < Luchadeer::Error; end
-    class Forbidden < Luchadeer::Error; end
     class NotFound < Luchadeer::Error; end
-    class RequestTimeout < Luchadeer::Error; end
     class InternalServerError < Luchadeer::Error; end
-    class BadGateway < Luchadeer::Error; end
-    class ServiceUnavailable < Luchadeer::Error; end
-    class GatewayTimeout < Luchadeer::Error; end
   end
 end
