bf746381d2a542b7bbfd93bf6d0bf87dec639f55
Paul Friedman <paulrfri@gmail.com>
2014-02-05 21:50:53 -0500

    Add top-level default client for each thread

--- lib/luchadeer.rb
+++ lib/luchadeer.rb
@@ -4,3 +4,19 @@ require 'luchadeer/api'
 require 'luchadeer/client'
 require 'luchadeer/error'
 require 'luchadeer/game'
+
+module Luchadeer
+  class << self
+    def configure(opts = {}, &blk)
+      Luchadeer.client = Luchadeer::Client.new(opts, &blk)
+    end
+
+    def client
+      Thread.current[:luchadeer_client]
+    end
+
+    def client=(new_client)
+      Thread.current[:luchadeer_client] = new_client
+    end
+  end
+end
