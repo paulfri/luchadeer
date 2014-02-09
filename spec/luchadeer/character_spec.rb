35c60dd66fee4e48ff26dc316e682694cd62801b
Paul Friedman <paulrfri@gmail.com>
2014-02-09 13:40:54 -0500

    Add missing specs: character and franchise finders

--- spec/luchadeer/character_spec.rb
+++ spec/luchadeer/character_spec.rb
@@ -6,4 +6,22 @@ describe Luchadeer::Character do
     expect(described_class.new).to be_a Luchadeer::Resource
   end
 
+  describe '.find' do
+    let(:client) { Luchadeer::Client.new }
+
+    before :each do
+      Luchadeer.client = client
+    end
+
+    it 'forwards to Client#character with arguments' do
+      expect(client).to receive(:character).with(1, true)
+      described_class.find(1, true)
+    end
+
+    it 'forwards to Client#character with default refresh' do
+      expect(client).to receive(:character).with(1, false)
+      described_class.find(1)
+    end
+  end
+
 end
--- spec/luchadeer/franchise_spec.rb
+++ spec/luchadeer/franchise_spec.rb
@@ -6,4 +6,22 @@ describe Luchadeer::Franchise do
     expect(described_class.new).to be_a Luchadeer::Resource
   end
 
+  describe '.find' do
+    let(:client) { Luchadeer::Client.new }
+
+    before :each do
+      Luchadeer.client = client
+    end
+
+    it 'forwards to Client#franchise with arguments' do
+      expect(client).to receive(:franchise).with(1, true)
+      described_class.find(1, true)
+    end
+
+    it 'forwards to Client#franchise with default refresh' do
+      expect(client).to receive(:franchise).with(1, false)
+      described_class.find(1)
+    end
+  end
+
 end
